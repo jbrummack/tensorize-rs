334d9d7dc7b8e6cceb4cf3cb22fc27415f5fce25
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2025-04-17 11:41:28 +0200

    Test jpg functionality

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ use tensorize_rs::{
 async fn main() -> anyhow::Result<()> {
     let resizer = ImageResizer::new(224, 224).await?;
     let img = image::open("image.png")?;
+    let img2 = image::open("image.jpg")?;
     resizer.rescale(&img, "rescaled.png").await?;
 
     let tensorizer = GpuTensorizer::new(IMAGENET_DEFAULT_CONFIG_NO_CROP).await?;
-    let tensor = tensorizer.tensorize(&img).await?;
+    let _ = tensorizer.tensorize(&img).await?;
+    let tensor = tensorizer.tensorize(&img2).await?;
     println!("{tensor:?}");
 
     let cpu = CpuTensorizer::default_no_crop().await?;
-    let cpu_tensor = cpu.tensorize(&img).await?;
+    let _ = cpu.tensorize(&img).await?;
+    let cpu_tensor = cpu.tensorize(&img2).await?;
     println!("{cpu_tensor:?}");
     Ok(())
 }
